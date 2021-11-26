f3b4db72efb5edc440225b9e42d41dabe0d8bb9c
Mahesh Ghamand <maheshghamand@gmail.com>
2021-11-27 00:05:16 +0530

    multisender fronted created

---
+++ backend/api/web3Provider.js
@@ -0,0 +1,5 @@
+export const getMultiSenderAddress = (_chainId) => {
+	if (_chainId === '0xfa2') return '0xCf92007F46Ca54C79B24Cb249104b4342fB2D0ce'
+	return null
+}
+
---
+++ hardhat.config.js
@@ -0,0 +1,40 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan");
+/* 
+npx hardhat verify --network fantomTestnet 
+0xf4910d212D6d6A5be64806e718dA038BC2392f0b
+*/
+require('dotenv').config()
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners()
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+})
+
+// npx hardhat deploy --network fantomTestnet --name "MultiSender"
+task('deploy', 'Deploy Contract')
+  .addParam('name', 'Contract Name')
+  .setAction(async (taskArgs, hre) => {
+    const [signer1] = await hre.ethers.getSigners()
+    const Contract = await hre.ethers.getContractFactory(taskArgs.name)
+    const contract = await Contract.deploy()
+    await contract.deployed();
+    console.log("Contract deployed to:", contract.address);
+  })
+
+
+module.exports = {
+  networks: {
+    fantomTestnet: {
+      url: `https://rpc.testnet.fantom.network/`,
+      accounts: [process.env.PRIVATE_KEY_0]
+    }
+  },
+  solidity: '0.8.2',
+  etherscan: {
+    apiKey: process.env.FANTOM_API_KEY
+  }
+}
